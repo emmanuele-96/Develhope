c55b7ee18c8935b887918e646d6c288523df7386
emmanuele-96 <emmanuelesartorio@gmail.com>
2024-02-21 10:15:29 +0100

    esercizio Ascii 2
    esercizio Ascii con due valori

--- casting1.java
+++ casting1.java
@@ -1,8 +1,18 @@
-public class casting1 {
+/*public class casting1 {
     public static void main(String args[]) {
         char q = '!';
         int e;
         e = q;
         System.out.println(e);
+    }
+}*/
+
+public class casting1 {
+    public static void main(String args[]) {
+        char a = '!';
+        char b = '>';
+        int c;
+        c = a + b;
+        System.out.println(c);
     }
 }
